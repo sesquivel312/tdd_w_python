8913fe9d9ea8371eb099cacd92696931b9ccdafd
Steve <retail.mbox@mgail.com>
2017-01-19 14:52:23 -0700

    fix functional test script, add some body text so a human can see that the page loaded

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-    return 'this is the home page'
+def home_page(request):
+
+    response_string = b'<html><title>To-Do lists</title><body>hooray</body></html>'
+
+    response = HttpResponse()
+    response.content = response_string
+
+    return response
