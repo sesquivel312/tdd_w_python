f3ac95d537b3b35e8af28b35f92485901e82b2dc
Steve <retail.mbox@mgail.com>
2017-01-23 09:40:09 -0700

    refactor view to use template

--- lists/views.py
+++ lists/views.py
@@ -3,9 +3,4 @@ from django.http import HttpResponse
 
 def home_page(request):
 
-    response_string = b'<html><title>To-Do lists</title><body>hooray</body></html>'
-
-    response = HttpResponse()
-    response.content = response_string
-
-    return response
+    return render(request, 'home.html')
