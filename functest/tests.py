62a3e3d236d5d1bbaf80e8727bf942317d03730d
Steve <retail.mbox@mgail.com>
2017-03-13 10:50:38 -0700

    update css
    added some bootstrap style items to: enlarge headers and text entry box, center everything,
    style table w/horizontal rules between rows, etc.
    
    also created a custom CSS thing? to move the input box a bit lower relative to the header

--- functest/tests.py
+++ functest/tests.py
@@ -6,11 +6,11 @@
 import time
 import unittest
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome('/home/steve/bin/chromedriver')
         self.browser.implicitly_wait(3)
